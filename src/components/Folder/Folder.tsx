6a6d5f0e3e5e4fdcad64119b6ff512a7b787ecd7
Shkatylo4ka <Shkatylo4ka@gmail.com>
2024-10-04 11:15:09 +0300

    expandednodes

--- src/components/Folder/Folder.tsx
+++ src/components/Folder/Folder.tsx
@@ -1,7 +1,39 @@
-import { IFolder } from "../../interfaces/folder.interface"
+import { useState } from 'react';
+import { INITIAL_DATA } from '../../assets/data';
+import { IFolder } from '../../interfaces/folder.interface';
 
-export function Folder(id: string, name: string, children: Array<IFolder> | undefined) {
-    return <div id={id}>
-        <img src="../../assets/openedFolder.svg" alt="Иконка папки"/>
-        {name}</div>
+const Folders = () => {
+  const [tree, setTree] = useState(INITIAL_DATA);
+  const [expandedNodes, setExpandedNodes] = useState([]);
+
+  const handleExpandCollapse = (e) => {
+    !expandedNodes.includes(e.currentTarget.dataset.id) ?
+    setExpandedNodes([...expandedNodes,e.currentTarget.dataset.id]) :
+    setExpandedNodes(expandedNodes.filter(node => node !== e.currentTarget.dataset.id));
+
+  }
+  
+  //setTree(document.querySelector('folders_tree')?.value)
+
+  const buildTree = (tree: IFolder[]) => {
+    tree.map((node) => {
+      const isExpanded = expandedNodes.includes(node.id)
+      return (
+          <div id={node.id}>
+            <img src={ isExpanded ? '../assets/openedFolder.svg' : '../assets/closedFolder.svg'} />
+            {node.name}
+            {node.children ? buildTree(node.children) : null}
+          </div>
+          
+          
+      )
+    })
+  }
+  return (
+    <div>
+      tree.length ? buildTree(tree) : 'Нет данных';
+    </div>
+  )
 }
+
+export default Folders
